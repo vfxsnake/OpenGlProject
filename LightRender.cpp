45cfa21811daf042b11882c91a99d1bba5271e6f
vfxsnake <jazzjoe@gmail.com>
2020-06-18 23:44:11 -0500

    chapter 6 Light class created. todo implement destructor.

---
+++ LightRender.cpp
@@ -0,0 +1,71 @@
+#include "LightRender.h"
+LightRender::LightRender(MeshType meshType, Camera* camera) // must be implemente to compile
+{
+	this->camera = camera;
+	
+	// choose the mesh type
+	switch (meshType)
+	{
+	case kTriangle:
+		Mesh::setTriData(vertices, indices); // initialize private varibles
+		break;
+
+	case kQuad:
+		Mesh::setQuadData(vertices, indices);
+		break;
+	
+	case kCube:
+		Mesh::setCubeData(vertices, indices);
+		break;
+
+	case kSphere:
+		Mesh::setSphereData(vertices, indices);
+		break;
+	}
+
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+	glBufferData(GL_ARRAY_BUFFER,  sizeof(Vertex) * vertices.size(), &vertices[0], GL_STATIC_DRAW);
+
+	// Enabiling vertex attributes array for attribute reading
+	glEnableVertexAttribArray(0);
+
+	// set the gl vertex Attribute pointer as the chain of attributes needs:
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (GLvoid*)0);
+	
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)(offsetof(Vertex, Vertex::color)));
+
+	glGenBuffers(1, &ebo);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * indices.size(), &indices[0], GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+}	
+
+void LightRender::draw()
+{
+	glm::mat4 model = glm::mat4(1.0f);
+	model = glm::translate(glm::mat4(1.0), position);
+	glUseProgram(this->program);
+	GLint modelLoc = glGetUniformLocation(program, "model");
+	
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+	glm::mat4 proj = camera->getProjectionMatrix();
+	GLint pLoc = glGetUniformLocation(program, "projection");
+	glUniformMatrix4fv(pLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
+	glBindVertexArray(vao);
+	glDrawElements(GL_TRIANGLES, indices.size(), GL_UNSIGNED_INT, 0);
+
+	glBindVertexArray(0);
+}
+
+// Todo implement destructor chapter6 p.189
---
+++ LightRender.h
@@ -0,0 +1,46 @@
+#pragma once
+#include <iostream>
+#include <GL/glew.h>
+// gl math includes
+#include "Dependencies/glm/glm/glm.hpp"
+#include "Dependencies/glm/glm/gtc/type_ptr.hpp"
+
+/* include developed clases this can be changed to forwar declaration latter including the structs done
+in the mesh class Vertex and MeshType
+*/
+#include "Mesh.h" // class Mesh;
+#include "ShaderLoader.h" // class ShaderLoader;
+#include "Camera.h" // class Camera;
+// struct Vertex;
+// struct MeshType
+
+class LightRender
+{
+public:
+	
+	LightRender(MeshType meshType, Camera* camera);
+	
+	~LightRender();
+
+	void draw();
+	void setPosition(glm::vec3 _position);
+	void setColor(glm::vec3 _color);
+	void setProgram(GLuint program);
+
+	glm::vec3 getPosition();
+	glm::vec3 getColor();
+
+private:
+	Camera* camera;
+	std::vector<Vertex>vertices; // variable to store vertex array
+	std::vector<GLuint>indices;; // variable to store index array
+	glm::vec3 position;
+	glm::vec3 color;
+	GLuint vbo; // vertex buffer object
+	GLuint ebo; //element buffer object
+	GLuint vao; // vertex array object
+	GLuint program;
+
+
+};
+
